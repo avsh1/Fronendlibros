3ed549d34c54dc32530bb5197d71669a0e8c93ca
Selena Ramón <selenaraquelramonchilulo@gmail.com>
2024-10-19 13:19:45 -0600

    Resuelto conflicto en NavMenu.razor y añadido libro

---
+++ ProyectoBibliotecaFrontend/Models/Libro.cs
@@ -0,0 +1,20 @@
+﻿namespace ProyectoBibliotecaFrontend.Models
+{
+    public class Libro
+    {
+
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public DateTime Publicacion { get; set; }
+
+        // Relaciones con otras entidades
+        public int Autor_Id { get; set; }
+        public Autor Autor { get; set; }
+
+        public int EditorialId { get; set; }
+        public Editorial Editorial { get; set; }
+
+        public int Categoria_Id { get; set; }
+        public Categoria Categoria { get; set; }
+    }
+}
