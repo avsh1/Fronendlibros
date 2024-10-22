5f2b606a1dae8d654fc586e7b5cc20ed97ca2899
Selena Ramón <selenaraquelramonchilulo@gmail.com>
2024-10-22 07:38:51 -0600

    actualizacion de cliente y editorial

--- ProyectoBibliotecaFrontend/Models/Cliente.cs
+++ ProyectoBibliotecaFrontend/Models/Cliente.cs
@@ -2,13 +2,13 @@
 {
     public class Cliente
     {
+
         public int Id { get; set; }
         public string Nombre { get; set; }
         public string Direccion { get; set; }
         public string Correo { get; set; }
-        public int Telefono { get; set; }
-        public string FechaRegistro { get; set; }
+        public string Telefono { get; set; }
+        public DateTime FechaRegistro { get; set; }
         public bool Estado { get; set; }
-
     }
 }
