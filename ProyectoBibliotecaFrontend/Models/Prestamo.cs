63f18d863101e386a4c7140b2c37a79db76c6417
Selena Ramón <selenaraquelramonchilulo@gmail.com>
2024-10-23 13:35:45 -0600

    Agregué la vista prestamo y actualice cliente

---
+++ ProyectoBibliotecaFrontend/Models/Prestamo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ProyectoBibliotecaFrontend.Models
+{
+    public class Prestamo
+    {
+        public int Id { get; set; }
+
+        // Relaciones con otras entidades
+        public int ClienteId { get; set; }
+        public Cliente Cliente { get; set; }
+
+        public int Libro_Id { get; set; }
+        public Libro Libro { get; set; }
+
+        // Atributos de préstamo
+        public string Status { get; set; }
+        public DateTime FechaPrestamo { get; set; }
+        public DateTime? FechaDevolucion { get; set; }  // FechaDevolucion puede ser nula si aún no se ha devuelto
+        public string Encargado { get; set; }
+    }
+}
